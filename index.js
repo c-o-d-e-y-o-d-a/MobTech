0841fcfce853be56588c19a91c8c7cd4b9089028
nischal gautam <nischalgautam7777@gmail.com>
2023-08-31 22:03:02 +0530

    site almost ready

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {       
+    setTimeout(function(){
+        document.getElementById('navbar').style.display='block';
+        document.getElementById('animation-mobile').style.display='none';
+    },5300); 
+}
